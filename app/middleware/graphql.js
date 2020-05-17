2f62d9d6df09dcc535cfcc96d415cca7e2d37f20
Hailong Wang <whlsxl@gmail.com>
2020-05-17 23:56:38 +0800

    feat: add playground

--- app/middleware/graphql.js
+++ app/middleware/graphql.js
@@ -2,8 +2,9 @@
 
 module.exports = (_, app) => {
   return async (ctx, next) => {
+    const { router } = app.config.graphql;
     if (app.graphqlServer) {
-      const middlewares = app.graphqlServer.getMiddleware();
+      const middlewares = app.graphqlServer.getMiddleware({ path: router });
       await middlewares(ctx, next);
     } else {
       await next();
